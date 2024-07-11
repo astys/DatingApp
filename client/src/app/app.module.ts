fe48195b4efc4a264f5c3c7b382332c7ec59303c
astys <ania.stys01@gmail.com>
2024-07-11 14:47:13 +0200

    End of section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,22 +9,36 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterOutlet} from '@angular/router';
+import { provideToastr } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    MemberListComponent,
+    ListsComponent,
+    MemberDetailComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     NavComponent,
+    RouterOutlet,
     BrowserAnimationsModule,
     RegisterComponent
   ],
   providers: [
-    provideAnimations()
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toat-bottom-right'
+    })
   ],
   bootstrap: [AppComponent]
 })
