8664ced31d46cb0ffe2c55adbeec14eb73024e83
astys <ania.stys01@gmail.com>
2024-11-12 23:04:16 +0100

    end of section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -26,23 +26,25 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
-            using var hmac = new HMACSHA512();
+            return Ok();
 
-            var user = new AppUser
-            {
-                UserName = registerDto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt = hmac.Key
-            };
+            // using var hmac = new HMACSHA512();
 
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+            // var user = new AppUser
+            // {
+            //     UserName = registerDto.Username.ToLower(),
+            //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            //     PasswordSalt = hmac.Key
+            // };
 
-            return new UserDto
-            {
-                Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
-            };
+            // _context.Users.Add(user);
+            // await _context.SaveChangesAsync();
+
+            // return new UserDto
+            // {
+            //     Username = user.UserName,
+            //     Token = _tokenService.CreateToken(user)
+            // };
         }
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,7 +7,16 @@ public class DataContext : DbContext
 {
     public DataContext(DbContextOptions options) : base(options)
     {
-        Database.EnsureCreated();
+        /*
+        Use EnsureCreated Carefully: The 
+        Database.EnsureCreated() method is
+        useful for creating the database and schema if
+        they do not exist.
+        However, it does not handle schema updates well.
+        If the table already exists, it will not recreate it.
+        Consider using migrations instead.
+        */
+        //Database.EnsureCreated();
     }
     public DbSet<AppUser> Users {get; set;}
 }
